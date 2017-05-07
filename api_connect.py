4c2bbad01cf5e70972db9695d867b0a0c996c0fd
Brady Mathieson <bradmath@umich.edu>
2017-05-07 17:06:35 -0500

    added api_connect.py so we can start interacting with the server

--- api_connect.py
+++ api_connect.py
@@ -11,9 +11,9 @@ Not sure how secure this server is... that's next on the list!
 import requests
 import json
 
-BASE_URL = "http://safe-messenger-server.herokuapp.com/v1"
-TEST_BASE = "http://127.0.0.1:5000/v1"
 HEADERS = {'Content-Type': 'application/json'}
+BASE_URL = "http://safe-messenger-server.herokuapp.com/v1"
+# TEST_BASE = "http://127.0.0.1:5000/v1"
 
 def add_user(username, ip, port):
     post_data = {
@@ -22,7 +22,7 @@ def add_user(username, ip, port):
         "port" : port
     }
 
-    req = requests.post(TEST_BASE+"/add_user", data=json.dumps(post_data), headers=HEADERS)
+    req = requests.post(BASE_URL+"/add_user", data=json.dumps(post_data), headers=HEADERS)
 
     if req.status_code == 400:
         raise Exception("Error: " + req.text)
@@ -34,7 +34,7 @@ def remove_user(username, ip, port):
         "port" : port
     }
 
-    req = requests.post(TEST_BASE+"/remove_user", data=json.dumps(post_data), headers=HEADERS)
+    req = requests.post(BASE_URL+"/remove_user", data=json.dumps(post_data), headers=HEADERS)
 
     if req.status_code == 400:
         raise Exception("Error: " + req.text)
